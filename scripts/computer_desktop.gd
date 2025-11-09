6957650dfcd5631f39f2bdfe243ed8eb0e9f3b61
rhaversen <rhaversen@gmail.com>
2025-11-10 00:02:00 +0100

    Remove unused files

--- scripts/computer_desktop.gd
+++
@@ -1,6 +0,0 @@
-extends "res://scripts/folder_race_minigame.gd"
-# Backward compatibility: This file now extends the folder race mini-game
-# To use the base desktop for other applications, extend "res://scripts/base_desktop.gd" instead
-
-# This file is kept for backward compatibility with existing scenes
-# All functionality is now in folder_race_minigame.gd which extends base_desktop.gd
